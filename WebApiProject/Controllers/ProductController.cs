40f8c92986ee8401848ebffc5a698a8900f219b2
Douglas Guzman <43095613+DouglasG96@users.noreply.github.com>
2021-09-22 10:00:19 -0600

    fin crud

--- WebApiProject/Controllers/ProductController.cs
+++ WebApiProject/Controllers/ProductController.cs
@@ -22,6 +22,35 @@ namespace WebApiProject.Controllers
             return products.Single(x => x.Id == id);
         } 
 
+        [HttpPost]
+        public ActionResult Create(Product model)
+        {
+            model.Id = products.Count() + 1;
+            products.Add(model);
+            return CreatedAtAction(
+                "Get",
+                new { id = model.Id },
+                model
+            );            
+        } 
+
+        [HttpPut("{productId}")]
+        public ActionResult Update(int productId, Product model)
+        {
+            var originalEntry = products.Single(x => x.Id == productId);
+            originalEntry.Name = model.Name;
+            originalEntry.Description = model.Description;
+            originalEntry.Price = model.Price;
+            return NoContent();
+        } 
+        
+        [HttpDelete("{idProduct}")]
+        public ActionResult Delete(int idProduct)
+        {
+            products = products.Where(x => x.Id != idProduct).ToList();
+            return NoContent();
+        } 
+
         public static List<Product> products = new List<Product>
         {
             new Product
